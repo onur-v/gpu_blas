1f1e48dc8647968abaf352cd71b4b61e14a85da9
Onur Vardar <vrdr.onur@gmail.com>
2025-04-23 01:26:53 +0300

    parameter based compilation script

--- make_cuda.sh
+++ make_cuda.sh
@@ -1,5 +1,5 @@
-nvcc custom_kernel_6.cpp -c -o custom_kernel_6.o -O3  -DRL$1 -ftz=true -use_fast_math -gencode=arch=compute_89,code=sm_89 -x cu
-nvcc custom_kernel_9.cpp -c -o custom_kernel_9.o -O3  -DRL$1 -ftz=true -use_fast_math -gencode=arch=compute_89,code=sm_89 -x cu
+nvcc custom_kernel_6.cpp -c -o custom_kernel_6.o -O3  -DRL$1 -DBLOCK_SIZE_X=$3 -DBLOCK_SIZE_Y=$4 -DMREG=$5 -DKLDS=$6 -DNLDS=$7 -DTN=$8 -DTK=$9 -DSX=${10} -ftz=true -use_fast_math -gencode=arch=compute_$2,code=sm_$2 -x cu
+nvcc custom_kernel_9.cpp -c -o custom_kernel_9.o -O3  -DRL$1 -DBLOCK_SIZE_X=$3 -DBLOCK_SIZE_Y=$4 -DMREG=$5 -DKLDS=$6 -DNLDS=$7 -DTN=$8 -DTK=$9 -DSX=${10} -ftz=true -use_fast_math -gencode=arch=compute_$2,code=sm_$2 -x cu
 nvcc cublas_bench.cpp -c -o cublas_bench.o -O3 -Xcompiler -fopenmp -DRL$1 -DRL$1 -Xcompiler -Wno-unused
 nvcc -o prog_cuda cublas_bench.o custom_kernel_6.o custom_kernel_9.o -L/usr/local/cuda/lib64 -lcublas -lopenblas -Xcompiler -fopenmp
 
